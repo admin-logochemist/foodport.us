739561fc5e377c2b468bf5a2e255fead64988c15
unknown <team@logochemist.com>
2022-07-13 22:21:22 +0500

    Alpha1

--- pages/user_sidebar.tsx
+++ pages/user_sidebar.tsx
@@ -15,9 +15,7 @@ function user_sidebar() {
                 <label htmlFor="scd_close-btn" className={`${sidestyle.scd_btn} ${sidestyle.scd_close_btns}`}><i className="fas fa-times"></i></label>
                <Link href="/your_orders"><li><a><i className="fas fa-shopping-cart"></i>Dashboard</a></li></Link>
                <Link href="/user_profile"><li><a><i className="far fa-address-card"></i>Profile</a></li></Link>
-               <Link href="/favourite"><li><a><i className="fas fa-bookmark"></i>Favourite</a></li></Link>
                <Link href="/Userorders"><li><a><i className="fas fa-sort-alpha-up"></i>Orders</a></li></Link>
-               <Link href="/"><li><a><i className="fas fa-cog"></i>Settings</a></li></Link>
                 </ul>
                 <label  htmlFor="scd_menu-btn" className={`${sidestyle.scd_btn} ${sidestyle.scd_menubtn}`}><i className="fas fa-bars"></i></label>
             </div>
